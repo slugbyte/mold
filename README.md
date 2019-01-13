d3ddc9a2d9792cd849a9232dab07cf1ef2bc6271
Duncan Marsh <slugbyte@slugbyte.com>
2019-01-13 09:11:55 +0800

    working on markdown to tui

--- README.md
+++ README.md
@@ -6,31 +6,32 @@
 * To install run `pip3 install `moldmoldmold`
 * Then run `mold --install` to setup your configuration directory
 
-## Usage 
+## Usage
 * get help with append `help`, `--help`, or `-h` to your command of interes
 * `mold help`, `mold -h`, and `mold --help` will print the general docs
-* subcommands also have specialized docs 
+* subcommands also have specialized docs
     * E.g. `mold temp --help` and `mold temp load --help`
 
-# NON-GOALS 
+## NON-GOALS
 * Adding support for os or hostname specic detection
-    * My [old mold like tools](https://github.com/slugbyte/mold/wiki/mold-prequels-and-their-lessons) had this feature, and I felt it over complicated the maintnece of my system configuration. 
+    * My [old mold like tools](https://github.com/slugbyte/mold/wiki/mold-prequels-and-their-lessons) had this feature, and I felt it over complicated the maintnece of my system configuration.
     * Instead plugs, confs, and execs can implament their own condional logic TODO: add link to my bashrc[example]()
 * Having the base install add premade configurration files
     * I don't follow the belief that systyem configurations can't be shared, because systems like [oh-my-zsh](https://ohmyz.sh/) work great for many people. However, molds goal is to help myself and others maintain their **personal** system configurations.
     * There is an option to Install from an existing mold_root on github, and I plan to make a *lite* oh-my-zsh like starter-kit mold_root repository at some point.
 
-## Development 
+## Development
 * Requires [pipenv](https://github.com/pypa/pipenv)
 * Clone code `git clone https://github.com/slugbyte/mold.git`
-* Inside the repo run `pipenv shell` to start the virutal env 
+* Inside the repo run `pipenv shell` to start the virutal env
 * Then run `source scripts/dev.sh`
   * this will create an alias calld `mold` that runs `python3 -m mold`
-  * the mold allias will be bound to an `\_mold` bash completion function 
-  * edit the code in mold/ and run `mold [OPTIONS] [FILENAME]` as you will 
+  * the mold allias will be bound to an `\_mold` bash completion function
+  * edit the code in mold/ and run `mold [OPTIONS] [FILENAME]` as you will
 * TODO: write integration tests
 
 # IDEAS?
-* (drop, plug, conf, exec) load -- suport for urls 
-* fold load -- suport for github repositorys ? -> submodule support? 
+* create a `name` task for renaming content
+* (drop, plug, conf, exec) load -- suport for urls
+* fold load -- suport for github repositorys ? -> submodule support?
 * Build a start mold_root for beginners to using a shell (a oh-my-zsh/bash lite)
--- mold/help/__init__.py
+++ mold/help/__init__.py
@@ -2,70 +2,138 @@
 help defines an api for printing mold help messages.
 '''
 
-import mold as _mold
-import mold.color as color
+import re
+import markdown
+import codecs 
+import mold.fs  as fs
+from mold.color import *
+
+
+
+def help_example():
+    conf_help = fs.dirname(__file__ ) + '/core/conf_help.md'
+    with codecs.open(conf_help, mode='r', encoding='utf-8') as help_file:
+        help_text = help_file.read()
+        to_print = markdown.markdown(help_text)
+    to_print = re.sub('\s+', ' ', to_print).strip() # strip whitespace
+
+        # help_text=help_file.read().replace('\n', '')
+    # to_print = markdown(help_text)
+    to_print = to_print.replace('<h1>', yellow).replace('</h1> ', reset + '\n')
+    to_print = to_print.replace('<h2>', yellow).replace('</h2> ', reset + '\n')
+    to_print = to_print.replace('<blockquote> <p>', '').replace('</p> </blockquote> ', reset + '\n')
+    to_print = to_print.replace('<p>', '').replace('</p> ', reset + '\n')
+    to_print = to_print.replace('<em>', magenta).replace('</em>', reset)
+    to_print = to_print.replace('<strong>', blue).replace('</strong>', reset)
+    to_print = to_print.replace('<br /> ', '\n')
+    to_print = to_print.replace('<code>', green).replace('</code> ', reset)
+    print(to_print)
+    # to_print = to_print.replace('<p>', '').replace('</p>', reset + '\n')
+    # to_print = to_print.replace('\n<code>', green).replace('</code>', reset + '')
+    # to_print = to_print.replace('<ul>\n', '').replace('\n</ul>', '')
+    # to_print = to_print.replace('<li>\n', '').replace('\n</li>', '')
+    # to_print = to_print.replace('<strong>', blue).replace('</strong>', reset)
+
+
+    # lines = to_print.split('\n')
+    # # to_print = ''
+    # result = ''
+    # for line in lines:
+        # if len(line) > 70:
+            # line_break_index = line.find(' ', 70)
+            # if line_break_index >= 0:
+                # line = line[:line_break_index] + '\n\t' + line[line_break_index + 1:]
+                # result = result + line + '\n'
+            # else:
+                # result = result +  line  + '\n'
+        # else:
+            # result = result + line + '\n'
+
+    # lines = result.split('\n')
+    # # to_print = ''
+    # result = ''
+    # for line in lines:
+        # if len(line) > 70:
+            # line_break_index = line.find(' ', 70)
+            # if line_break_index >= 0:
+                # line = line[:line_break_index] + '\n\t' + line[line_break_index + 1:]
+                # result = result + line + '\n'
+            # else:
+                # result = result +  line  + '\n'
+        # else:
+            # result = result + line + '\n'
+
+    # result = result.replace('.i\n\n', '\n')
+        # print(len(line))
+        # print(line.find('--'))
+
+    # print(result.strip())
+
+
+# import mold as _mold
+# import mold.colokr as color
 
 # load all the help files
-import mold.help.main_help as main_help 
+# import mold.help.main_help as main_help 
 
-# core 
-import mold.help.core.conf_help as conf_help
-import mold.help.core.plug_help as plug_help
-import mold.help.core.exec_help as exec_help
-import mold.help.core.drop_help as drop_help
-import mold.help.core.fold_help as fold_help
-import mold.help.core.core_task_help as core_task_help
+# # core 
+# import mold.help.core.conf_help as conf_help
+# import mold.help.core.plug_help as plug_help
+# import mold.help.core.exec_help as exec_help
+# import mold.help.core.drop_help as drop_help
+# import mold.help.core.fold_help as fold_help
+# import mold.help.core.core_task_help as core_task_help
 
-# sync 
-import mold.help.sync.sync_help as sync_help
+# # sync 
+# import mold.help.sync.sync_help as sync_help
 
-# TODO NOW: 1) get the rest of the help text in this file into own files
-# 2) make help text files for sync tasks
-# 3) figure out how you want to really go about compile and color <3
+# # TODO NOW: 1) get the rest of the help text in this file into own files
+# # 2) make help text files for sync tasks
+# # 3) figure out how you want to really go about compile and color <3
 
-# TODO: refacter each help to be a string
-# then create a comple fiunction that will choose to colorify base on ctx
-# then make a create_help_handler to generate help defs (functions)
+# # TODO: refacter each help to be a string
+# # then create a comple fiunction that will choose to colorify base on ctx
+# # then make a create_help_handler to generate help defs (functions)
 
-header_color = color.cyan 
-task_color = color.blue
-command_color = color.magenta
-warning_color = color.red 
-reset = color.reset
+# header_color = color.cyan 
+# task_color = color.blue
+# command_color = color.magenta
+# warning_color = color.red 
+# reset = color.reset
 
 
-def colorify(str):
-    for header in ['USAGE:', 'ABOUT:', 'INSTALL:', 'CONFIGURATION:', 'HELP:', 'COMMANDS:', 
-            'TASKS:', 'NOTE:', 'INSTALL FROM REPOSITORY:']:
-        str = str.replace(header, f'{header_color}{header}{reset}')
-    for command in ['conf:', 'plug:', 'exec:', 'drop:', 'fold:', 'sync:']:
-        str = str.replace(command, f'{command_color}{command.replace(":", "")}{reset}')
-    for task in [' list:', ' make:', ' load:', ' edit:', ' dump:', ' nuke:', 
-            ' add:', ' commit:', ' push:', ' merge:', ' auto:', ' log:', ' pull:',
-            ' status:', ' branch:', ' diff:', ' --force-push:', ' --hard-reset:', ' --new-branch:', 
-            ' --delete-branch:', ' --checkout:']:
-        str = str.replace(task, f'{task_color}{task.replace(":", "")}{reset}')
-    for warn in ['WARNING:', 'DANGER:']: 
-        str = str.replace(warn, f'{warning_color}{warn}{reset}')
-    return str
+# def colorify(str):
+    # for header in ['USAGE:', 'ABOUT:', 'INSTALL:', 'CONFIGURATION:', 'HELP:', 'COMMANDS:', 
+            # 'TASKS:', 'NOTE:', 'INSTALL FROM REPOSITORY:']:
+        # str = str.replace(header, f'{header_color}{header}{reset}')
+    # for command in ['conf:', 'plug:', 'exec:', 'drop:', 'fold:', 'sync:']:
+        # str = str.replace(command, f'{command_color}{command.replace(":", "")}{reset}')
+    # for task in [' list:', ' make:', ' load:', ' edit:', ' dump:', ' nuke:', 
+            # ' add:', ' commit:', ' push:', ' merge:', ' auto:', ' log:', ' pull:',
+            # ' status:', ' branch:', ' diff:', ' --force-push:', ' --hard-reset:', ' --new-branch:', 
+            # ' --delete-branch:', ' --checkout:']:
+        # str = str.replace(task, f'{task_color}{task.replace(":", "")}{reset}')
+    # for warn in ['WARNING:', 'DANGER:']: 
+        # str = str.replace(warn, f'{warning_color}{warn}{reset}')
+    # return str
 
-def color_print(*args):
-    color_args = []
-    for x in args:
-        color_args.append(colorify(x))
-    print(*color_args)
+# def color_print(*args):
+    # color_args = []
+    # for x in args:
+        # color_args.append(colorify(x))
+    # print(*color_args)
 
-def compile(ctx, text):
-    green = color.green
-    reset = color.reset
-    mold = _mold
-    return text.format(**locals())
+# def compile(ctx, text):
+    # green = color.green
+    # reset = color.reset
+    # mold = _mold
+    # return text.format(**locals())
 
-def main(ctx):
-    color_print(compile(ctx, main_help.text))
-def conf(ctx):
-    color_print(compile(ctx, conf_help.text))
-def plug(ctx):
-    color_print(compile(ctx, conf_plug.text))
+# def main(ctx):
+    # color_print(compile(ctx, main_help.text))
+# def conf(ctx):
+    # color_print(compile(ctx, conf_help.text))
+# def plug(ctx):
+    # color_print(compile(ctx, conf_plug.text))
 
 
